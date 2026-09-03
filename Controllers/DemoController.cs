bbe241172c8d0da54667ae588d2b32020441b35b
Hung <nguyenhung161003@gmail.com>
2026-09-03 12:51:49 +0700

    Routing

--- Controllers/DemoController.cs
+++ Controllers/DemoController.cs
@@ -10,3 +10,21 @@ public class Demo : Controller
     {
         return View();
     }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Edit(int id)
+    {
+      return Content("ID = " + id);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
--- obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30bc93de95c357dd09c37274383e9798097a8114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6179e530c8aa9aa799cf4a1cc13334ff51f6badc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
